0be25c392bb2d51566c751dffadc13dd07964a3e
Ryan Mark <rymark907@gmail.com>
2021-11-11 14:55:51 -0800

    finished up with server tests

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -12,6 +12,31 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+router.get('/:id', async (req, res, next) => {
+    try {
+        const car = await Cars.getById(req.params.id)
+        res.status(200).json(car)
+    } catch (err) {
+        next(err)
+    }
+})
 
+router.post('/', async (req, res, next) => {
+    try {
+        const newCar = await Cars.insert(req.body)
+        res.status(201).json(newCar)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const updatedCar = await Cars.update(req.params.id, req.body)
+        res.status(200).json(updatedCar)
+    } catch (err) {
+        next(err)
+    }
+})
 
 module.exports = router
--- api/server.test.js
+++ api/server.test.js
@@ -16,7 +16,78 @@ afterAll(async () => {
 })
 
 describe('[GET] /cars', () => {
-    test('responds with all hobbits', async () => {
-        expect(true).toBeTruthy()
+    test('responds with all cars', async () => {
+        const resp = await request(server).get('/api/cars')
+        expect(resp.body).toHaveLength(4)
+    })
+    test('responds with 200 status', async () => {
+        const resp = await request(server).get('/api/cars')
+        expect(resp.status).toBe(200)
+    })
+})
+
+describe('[GET] /cars/:id', () => {
+    test('responds with car at the particular id', async () => {
+        const resp = await request(server).get('/api/cars/1')
+        expect(resp.body).toMatchObject({
+            "id": 1,
+            "make": "Tesla",
+            "model": "Model 3",
+            "year": "2020"
+        })
+    })
+    test('responds with 200 status', async () => {
+        const resp = await request(server).get('/api/cars/1')
+        expect(resp.status).toBe(200)
+    })
+})
+
+describe('[POST] /cars', () => {
+    test('responds with newly posted car', async () => {
+        const resp = await request(server).post('/api/cars')
+        .send({
+            "make": "Chevrolet",
+            "model": "Corvette Stingray",
+            "year": "2022"
+        })
+        expect(resp.body).toMatchObject({
+            "make": "Chevrolet",
+            "model": "Corvette Stingray",
+            "year": "2022"
+        })
+    })
+    test('responds with 201 status', async () => {
+        const resp = await request(server).post('/api/cars')
+        .send({
+            "make": "Chevrolet",
+            "model": "Corvette Stingray",
+            "year": "2022"
+        })
+        expect(resp.status).toBe(201)
+    })
+})
+
+describe('[PUT] /cars/:id', () => {
+    test('responds with newly updated car', async () => {
+        const resp = await request(server).put('/api/cars/1')
+        .send({
+            "make": "Tesla",
+            "model": "Model S Plaid",
+            "year": "2022"
+        })
+        expect(resp.body).toMatchObject({
+            "make": "Tesla",
+            "model": "Model S Plaid",
+            "year": "2022"
+        })
+    })
+    test('responds with 200 status', async () => {
+        const resp = await request(server).put('/api/cars/1')
+        .send({
+            "make": "Tesla",
+            "model": "Model S Plaid",
+            "year": "2022"
+        })
+        expect(resp.status).toBe(200)
     })
 })
